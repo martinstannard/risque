43cb92a650159b615fac38f6d2841406bf09b390
Matt Allen <matt.allen@gmail.com>
2008-11-15 16:30:08 +1030

    Games controllers

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,8 +10,16 @@ class GamesController < ApplicationController
     g = Game.new
     g.world = World.begat
     g.save!
-    g.players << Player.find(:all,:limit => 4)
+    g.players << Player.find(:all,:limit => 4).sort_by{ rand }
+    g.allocate_countries
+    g.allocate_initial_armies
+    g.current_player = g.players.first.id
     redirect_to game_path(g)
     return false 
+  end
+  
+  def game
+    @game = Game.find(params[:id])
+    @player = @game.current_player
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,28 @@ class Game < ActiveRecord::Base
   has_many :game_players
   has_many :players, :through => :game_players
   belongs_to :world
+  has_one :current_player, :class => "Player", :foreign_key => "current_player"
+  
+  def allocate_countries
+    countries = []
+    world.regions.each do |r|
+      r.countries.each do |c|
+        countries << c
+      end    
+    end
+    countries = countries.sort_by{ rand }
+    countries.in_groups_of(game_players.size).each do |group|
+      group.each_with_index do |country,index|
+        GamePlayerCountry.create!(:game_player_id => game_players[index].id,:country_id => country.id, :armies => 1)
+      end
+    end
+  end
+  
+  def allocate_initial_armies
+    game_players.each do |gp|
+      gp.armies_to_allocate = gp.game_player_countries.count
+      gp.save
+    end
+  end
   
 end
