0ee87a48ef36e3ae5dd23f9cb6c79f83e5cb714a
Martin Stannard <mstannard@gmail.com>
2008-11-15 21:19:41 +1100

    added attack method - allows one country to attack another with a variable number of armies and subtracts armies from game_player_country according to who wins

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,5 @@
 class Country < ActiveRecord::Base
-  
+
   belongs_to :region
   has_many :neighbours, :dependent => :destroy
   has_one :game_player_country
@@ -9,7 +9,26 @@ class Country < ActiveRecord::Base
     "country_#{id}"
   end
 
-  def attack(target)
+  def attack(target, attacker_dice = 1, defender_dice = 1)
+    results = roller(attacker_dice, defender_dice)
+    results.each do |r|
+      r ?  target.game_player_country.add_armies(-1) : game_player_country.add_armies(-1)
+    end
+  end
+
+  protected
+
+  def roller(attacker_rolls = 1, defender_rolls = 1)
+    attack_die = (0...attacker_rolls).collect { |r| dice }.sort[0...defender_rolls].flatten
+    defend_die = (0...defender_rolls).collect { |r| dice }.sort.flatten
+    puts "#{attack_die} - #{defend_die}"
+    results = []
+    defender_rolls.times { |r| results << (attack_die[r] > defend_die[r]) }
+    results
+  end
+
+  def dice
+    rand(6) + 1
   end
 
 end
--- app/models/game_player_country.rb
+++ app/models/game_player_country.rb
@@ -3,4 +3,8 @@ class GamePlayerCountry < ActiveRecord::Base
   belongs_to :country
   belongs_to :game_player
 
+  def add_armies(count = 1)
+    update_attribute(:armies, armies + count)
+  end
+
 end
