b463130ac3721de27db23b052fbd335203d6f2d9
Matt Allen <matt.allen@gmail.com>
2008-11-15 14:48:55 +1030

    Gamin'

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,15 @@
 class GameController < ApplicationController
   def new
+    
   end
 
   def show
+  end
+  
+  def create
+    g = Game.new
+    g.world = World.begat
+    g.save
+    g.players << Player.find(:all,:limit => 4)
   end
 end
