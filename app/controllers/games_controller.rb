7037d4bfc931bf961c2f0af7e7d2440259e52816
Phil Oye <philoye@philoye.com>
2008-11-15 22:33:45 +1030

    Fixed allocate armies method.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -25,6 +25,15 @@ class GamesController < ApplicationController
     render :partial => "allocate"
   end
 
+  def attack
+    @game_player_attacking_country = GamePlayerCountry.find(:first, :conditions =>["game_player_id = ? and country_id = ?",params[:game_player_id],params[:attacker_country_id]])
+    @game_player_target_country = GamePlayerCountry.find(:first, :conditions =>["country_id = ?",params[:target_country_id]])
+    @armies = params[:armies].to_i
+    
+    @game_player_attacking_country.attack(@game_player_target_country,@armies)
+    render :partial => "attack", :layout => false
+  end
+
   def allocate_armies
     @game_player_country = GamePlayerCountry.find(:first, :conditions =>["game_player_id = ? and country_id = ?",params[:game_player_id],params[:country_id]])
     @game_player_country.armies += params[:armies].to_i
@@ -35,11 +44,13 @@ class GamesController < ApplicationController
     @game = @game_player.game
     @game_player = @game_player.game.get_game_player
     if @game_player.nil? && @game.is_allocation_round?
-      @game_player = @game.game_player.find(:first,:conditions => "id ASC")
-      @game.is_allocation_round = false
-      @game.save
+      @game_player = @game.game_players.find(:first,:order => "id ASC")
+      @game.is_allocation_round = 0
+      @game.save!
+      render :partial => "attack", :layout => false
+    else
+      render :partial => "allocate", :layout => false
     end
-    render :partial => "allocate", :layout => false
   end
 
 end
